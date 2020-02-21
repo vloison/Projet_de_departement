dafe2bdf53eea92ce570f004983fda04d45cbbbb
Maxime Poli <maxime.poli@eleves.enpc.fr>
2020-02-21 22:00:30 +0100

    preprocessing data

--- database.py
+++ database.py
@@ -1,42 +1,44 @@
-import pandas as pd
-import numpy as np
+"""Downloads the posters and cleans the database"""
 import os
-from tqdm import tqdm  # barre de chargement
 from urllib.request import urlretrieve
+import pandas as pd
+from tqdm import tqdm  # barre de chargement
 
 
 def database_download(savelocation, dataset):
+    """Downloads the database from the given links in the dataset"""
     if not os.path.exists(savelocation):
         os.mkdir(savelocation)
     not_found = []
-    postersURLS = dataset['poster'].to_list()
+    posters_urls = dataset['poster'].to_list()
     print('Posters database downloading')
-    for index in tqdm(range(len(postersURLS))):
+    for index in tqdm(range(len(posters_urls))):
         jpgname = savelocation+str(index)+'.jpg'
         try:
             if not os.path.isfile(jpgname):
-                urlretrieve(postersURLS[index], jpgname)
-        except:
+                urlretrieve(posters_urls[index], jpgname)
+        except Exception:  # Mettre le nom de l'exception possible
             not_found.append(index)
     print('Database downloaded')
     return not_found
 
 
-savelocation = 'posters/'
-raw_movies = pd.read_csv('poster_data.csv')
-today = pd.Timestamp.today()
+SAVELOCATION = 'posters/'
+RAW_MOVIES = pd.read_csv('poster_data.csv')
+TODAY = pd.Timestamp.today()
 
-# On nettoie la base de données en ne gardant que les films ayant un titre,
-# une date de sortie, au moins un genre et un poster.
-# On enlève les films pas encore sortis.
-movies = raw_movies.dropna(
+'''
+On nettoie la base de données en ne gardant que les films ayant un titre,
+une date de sortie, au moins un genre et un poster.
+On enlève les films pas encore sortis.
+'''
+MOVIES = RAW_MOVIES.dropna(
     subset=['title', 'release_date', 'genre_1', 'poster']).drop(
-        raw_movies[raw_movies['release_date'].map(pd.Timestamp) > today].index)
-# movies.to_csv('out.csv')
+        RAW_MOVIES[RAW_MOVIES['release_date'].map(pd.Timestamp) > TODAY].index)
 
 
 def genre_count(movies):
-    # Liste des genres répertoriés
+    """Prints the genres (first category) and the number of appearences"""
     genre_list = movies.genre_1.unique()
 #     for genre in np.append(movies.genre_2.unique(), movies.genre_3.unique()):
 #         if genre not in genre_list:
@@ -46,4 +48,12 @@ def genre_count(movies):
         print(label, occurences)
 
 
-genre_count(movies)
+genre_count(MOVIES)
+'''
+Voir quels genres supprimer
+Choisir un échantillon d'entraînement à partir des films restants
+Pas forcément tout faire au hasard: on a un training set où certains genres
+sont bien plus représentés.
+'''
+MOVIES.to_csv('clean_poster_data.csv')
+# database_download(savelocation, movies)
---
+++ preprocessing.py
@@ -0,0 +1,75 @@
+"""Preprocess the input data before giving it to the neural network"""
+import glob
+import imageio
+import skimage
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+SAVELOCATION = 'posters/'
+MOVIES = pd.read_csv('clean_poster_data.csv')
+
+
+def get_id(filename):
+    """Get the id from the filename"""
+    index_s = filename.rfind("/") + 1
+    index_f = filename.rfind(".jpg")
+    return int(filename[index_s:index_f])
+
+
+def show_img(dataset, posters, labels, ids, index):
+    """Show the image with id at index position in ids"""
+    title = dataset.at[ids[index], 'title']
+    genre = dataset.at[ids[index], 'genre_1']
+#     genres = [dataset.at[id,'genre_'+i] for i in ['1','2','3']]
+    plt.imshow(posters[index])
+    plt.title("{} \n {}".format(title, genre))
+    print('Indice du film: ', ids[index])
+    print('Genre conservé: ', labels[index])
+    plt.show()
+
+
+def preprocess(img, size=(150, 100, 3)):
+    """Normalize the image"""
+    img = skimage.transform.resize(img, size)
+    img = img.astype(np.float32)
+#     img = (img / 127.5) -1
+    return img
+
+
+def prepare_data(path, dataset, size=(150, 100, 3), save=True):
+    """Generate the data to be used by the neural network"""
+    print('Generating dataset...')
+    genre_list = dataset.genre_1.unique()
+    nb_genres = len(genre_list)
+    inv_genre = {genre_list[k]: k for k in range(nb_genres)}
+    image_glob = glob.glob(path + "*.jpg")
+    posters, genres, ids = [], [], []
+    for filename in tqdm(image_glob):
+        try:
+            # Meilleure gestion d'erreur à faire
+            posters.append(preprocess(imageio.imread(filename), size))
+            index = get_id(filename)
+            vect_genre = np.zeros(nb_genres)
+            # Rajoute un 1 à l'indice correspondant à la position
+            # du premier genre de ce film dans la liste des genres
+            vect_genre[inv_genre[dataset.at[index, 'genre_1']]] = 1
+            ids.append(index)
+            genres.append(vect_genre)
+        except Exception as e:
+            print(e)
+    if save:
+        np.save('numpy_posters', posters)
+        np.save('numpy_genres', genres)
+        np.save('numpy_ids', ids)
+    return posters, genres, ids
+
+
+X,Y,ID = prepare_data(SAVELOCATION, MOVIES)
+# X = np.load('numpy_posters.npy')
+# Y = np.load('numpy_genres.npy')
+# ID = np.load('numpy_ids.npy')
+show_img(MOVIES, X, Y, ID, 68)
+print(MOVIES.genre_1.unique())
