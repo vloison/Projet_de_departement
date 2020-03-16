c4e625cb510e7c25e54f3499ab555f37670867ab
Maxime Poli <maxime.poli@eleves.enpc.fr>
2020-03-16 13:29:22 +0100

    visualisation resultats

--- moviegenre/CNN.py
+++ moviegenre/CNN.py
@@ -1,10 +1,12 @@
 import numpy as np
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
-from tensorflow.keras.optimizers import Adagrad
-from tensorflow.keras.metrics import Accuracy
+import pandas as pd
+import matplotlib.pyplot as plt
+from preprocessing import show_img
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Dropout, Flatten
+# from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+# from tensorflow.keras.optimizers import Adagrad
+# from tensorflow.keras.metrics import Accuracy
 from tensorflow.keras.models import load_model
 
 # model = Sequential()
@@ -28,11 +30,61 @@ from tensorflow.keras.models import load_model
 model = load_model('../data/first_model.h5')
 Xtr = np.load("../data/sets/Xtr_tr=2000_test=100.npy")
 Ytr = np.load("../data/sets/Ytr_tr=2000_test=100.npy")
+IDStr = np.load("../data/sets/IDStr_tr=2000_test=100.npy")
 
 Xtest = np.load("../data/sets/Xtest_tr=2000_test=100.npy")
 Ytest = np.load("../data/sets/Ytest_tr=2000_test=100.npy")
+IDStest = np.load("../data/sets/IDStest_tr=2000_test=100.npy")
 
+MOVIES = pd.read_csv("../data/clean_poster_data.csv")
+class_names = MOVIES.genre_1.unique()
 # model.fit(Xtr, Ytr, batch_size=16, epochs=5, verbose=1, validation_split=0.1)
 
-model.evaluate(Xtest, Ytest, batch_size=1, verbose=1)
+# model.evaluate(Xtest, Ytest, batch_size=1, verbose=1)
+predictions = model.predict(Xtest)
+print(predictions[0])
 # model.save('../data/first_model.h5')
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == np.argmax(true_label):
+      color = 'blue'
+    else:
+      color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[np.argmax(true_label)]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(17), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[np.argmax(true_label)].set_color('blue')
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i+20, predictions[i+20], Ytest, Xtest)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i+20, predictions[i+20], Ytest)
+plt.tight_layout()
+plt.show()
+
