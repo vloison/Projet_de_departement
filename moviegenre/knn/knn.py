2a4b7195a1e0e43bb66446d891a062161879c862
Maxime Poli <maxime.poli@eleves.enpc.fr>
2020-04-29 14:04:47 +0200

    plus que les trucs de Nico

--- moviegenre/knn/knn.py
+++ moviegenre/knn/knn.py
@@ -7,18 +7,23 @@ class KNN(KNeighborsClassifier):
     def __init__(self, n_neighbors, verbose=True, weights='distance'):
         super().__init__(n_neighbors=n_neighbors, weights=weights)
         self.verbose = verbose
+        self.neighbors = None
 
     def predict(self, test_features, train_genres):
+        self.neighbors = []
         generator = tqdm(range(len(test_features))) if self.verbose else range(len(test_features))
         predicted_genres = np.zeros((len(test_features), train_genres.shape[1]))
         if self.verbose:
             print('Predicting...')
         for i in generator:
             neighbors = self.kneighbors([test_features[i]], return_distance=False)
+            self.neighbors.append(neighbors)
             label_for_max = train_genres[neighbors]
             label_for_max = np.sum(label_for_max, axis=1)
             ind_genre = np.argmax(label_for_max)
             predicted_genres[i][ind_genre] = 1
+        self.neighbors = np.array(self.neighbors)
+        self.neighbors = self.neighbors[:,0,:]
         return predicted_genres    
 
 
--- moviegenre/utils/display/results_display.py
+++ moviegenre/utils/display/results_display.py
@@ -12,25 +12,31 @@ def show_img(dataset, posters, labels, ids, index):
     plt.show()
 
 
-def plot_image(img, ground_truth_array, class_names, predictions_array):
+def plot_image(img, ground_truth_array, class_names, predictions_array, in_test=True):
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
 
     plt.imshow(img, cmap=plt.cm.binary)
 
-    predicted_label = np.argmax(predictions_array)
     true_label = np.argmax(ground_truth_array)
-    if predicted_label == true_label:
-        color = 'blue'
+    predicted_label = np.argmax(predictions_array)
+    if in_test:
+        if predicted_label == true_label:
+            color = 'blue'
+        else:
+            color = 'red'
+
+        plt.xlabel("{} {:2.0f}% \n({})".format(class_names[predicted_label],
+            100*np.max(predictions_array),
+            class_names[true_label]),
+            color=color) 
     else:
-        color = 'red'
-
-    plt.xlabel("{} {:2.0f}% \n({})".format(class_names[predicted_label],
-        100*np.max(predictions_array),
-        class_names[true_label]),
-    color=color)
-
+        if predicted_label == true_label:
+            color = 'blue'
+        else:
+            color = 'black'
+        plt.xlabel(class_names[true_label], color=color)
 
 def plot_value_array(ground_truth_array, predictions_array):
     plt.grid(False)
@@ -56,3 +62,69 @@ def plot_test_results(test_posters, test_genres, class_names, predicted_genres, 
         plot_value_array(test_genres[i], predicted_genres[i])
 
     plt.tight_layout()
+
+
+def plot_neighbors(test_posters, test_genres, class_names, predicted_genres, starting_index, num_images, train_posters, train_genres, neighbors):
+    
+    k = len(neighbors[0])
+    num_images = min(num_images, len(test_posters)-starting_index)
+    plt.figure(figsize=(2 * (k+1), 2 * num_images))
+    ind = 0
+    for i in range(starting_index, starting_index+num_images):
+        ind += 1
+        plt.subplot(num_images, k+1, ind)
+        plot_image(test_posters[i], test_genres[i], class_names, predicted_genres[i])
+        for n_neighbor in range(k):
+            ind += 1
+            plt.subplot(num_images, k+1, ind)
+            plot_image(train_posters[neighbors[i, n_neighbor]], train_genres[neighbors[i, n_neighbor]], class_names, test_genres[i], in_test=False)
+
+    plt.tight_layout()
+
+
+# def histogram(ground_truth_arrays, predicted_arrays, genre_index, title):
+#     predicted_labels = np.argmax(predictions_arrays, axis=1)
+#     true_labels = np.argmax(ground_truth_arrays, axis=1)
+#     to_keep = np.argwhere(true_labels == genre_index)
+#     predicted_labels = predicted_labels[to_keep]
+#     true_labels = true_labels[to_keep]
+
+
+def histogram(test_genres, predicted_genres, kneighbors, genres, save=False):
+    genres_inv = {genres[k]: k for k in genres.keys()}
+    predictions = np.array([genres_inv[k] for k in np.argmax(predicted_genres, axis=1)])
+    ground_truth = np.array([genres_inv[k] for k in np.argmax(test_genres, axis=1)])
+
+    # results_per_genre : matrice donc les lignes sont les vrais genres, et les colonnes sont les genres prédits
+    results_per_genre = {
+        genre_true : {genre_pred : 0 for genre_pred in genres}
+        for genre_true in genres
+    }
+    #print("results per genre", results_per_genre)
+    #print('results per genre ligne', results_per_genre['Action'].values())
+    # total_per_genre : vecteur qui comptabilise le nombre de représentants de chaque genre
+    total_per_genre = {
+        genre : 0
+        for genre in genres
+    }
+    # Mise à jour de results_per_genre et total_per_genre en fonction des prédictions
+    n = len(predictions)
+    for i in range(n):
+        results_per_genre[ground_truth[i]][predictions[i]] += 1
+        total_per_genre[ground_truth[i]] += 1
+
+    # Accuracy
+    accuracy = 0
+    for genre in genres:
+        accuracy += results_per_genre[genre][genre]
+    accuracy /= len(test_genres)
+
+    # Visualisation:
+    genres_list = list(config['genres'])
+    for iterateur in genres_list:
+        plt.figure(figsize=(15, 5))
+        plt.title('Prédictions sur les films de genre ' + iterateur + ' k='+str(k) +", Accuracy totale:" + str(accuracy))
+        plt.bar(genres_list, results_per_genre[iterateur].values())
+        plt.show()
+        if save:
+            plt.savefig('../results/Resnet+kNN/'+iterateur+'_k='+str(kneighbors)+'.png')
